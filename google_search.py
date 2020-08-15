06e288a8250866379291777d077e9192e19b76c7
shubhakanha <shubhamsharma1981996@gmail.com>
2020-08-15 19:33:46 +0530

    google search result get

--- google_search.py
+++ google_search.py
@@ -1,14 +1,8 @@
-# from googlesearch import search
+from googlesearch import search
 
 
 def search_from_google(query):
     result_list = []
-    # for i in search(query, tld='com', lang='en', num=5, start=0, stop=5, pause=1.0):
-    #     result_list.append(i)
-
-    result_list = ['https://www.java.com/', 'https://www.oracle.com/java/technologies/',
-                   'https://www.lifewire.com/what-is-java-4172382',
-                   'https://en.wikipedia.org/wiki/Java_(programming_language)',
-                   'https://www.oracle.com/in/java/technologies/javase-downloads.html']
-
+    for i in search(query, tld='com', lang='en', num=5, start=0, stop=5, pause=1.0):
+        result_list.append(i)
     return result_list
