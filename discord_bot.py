ec33ee9784d19f49973e836bbce77778edf79e7f
ShubhamSharmaGo <shubham.sharma1@go-mmt.com>
2020-08-15 15:29:31 +0530

    google search result return

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,5 @@
 import discord
+from google_search import search_from_google
 
 client = discord.Client()
 
@@ -9,6 +10,20 @@ async def on_message(message):
         return
     if message.content.startswith('hi'):
         await message.channel.send('Hey')
+
+    if message.content.startswith('!google'):
+        query = message.content.split(None, 1)[1]
+        author_id = message.author.id
+        # post_search_data(author_id, query)
+
+        results = search_from_google(query)
+        if results:
+            links = ' \n'.join(results)
+            msg = 'Hello {}, you searched for {}. The top five results are: \n {}'.format( message.author.mention, query, links)
+        else:
+            msg = 'Hello {}, you searched for {}. \n Sorry, no matching links found.'.format(message.author.mention, query)
+
+        await message.channel.send(msg)
 
 token = ''
 
